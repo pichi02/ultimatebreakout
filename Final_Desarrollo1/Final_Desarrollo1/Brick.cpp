a46722b08ef5728076725ffd1f9ff06b486c5369
pichi02 <lucas.pich02@gmail.com>
2022-02-16 00:10:03 -0300

    creacion de clase Brick para los ladrillos

---
+++ Final_Desarrollo1/Final_Desarrollo1/Brick.cpp
@@ -0,0 +1,86 @@
+#include "Brick.h"
+
+Brick::Brick(float width, float height, Vector2 pos)
+{
+	this->pos = pos;
+	this->width = width;
+	this->height = height;
+	isActive = true;
+}
+
+Brick::~Brick()
+{
+}
+
+void Brick::CheckBallCollision(Ball* ball)
+{
+	Vector2 ballPosition = { ball->GetPosition().x + ball->GetWidth() / 2, ball->GetPosition().y + ball->GetHeight() / 2 };
+	if (isActive)
+	{
+		if (CheckCollisionRecs(Rectangle{ ball->GetPosition().x,ball->GetPosition().y,ball->GetWidth(),ball->GetHeight() }, Rectangle{ pos.x,pos.y,width,height }))
+		{
+			Vector2 oppositeCorner = { pos.x + width, pos.y + height };
+			isActive = false;
+			if (ballPosition.x < pos.x)
+			{
+				if (ball->GetSpeed().x > 0)
+				{
+					ball->ReverseXSpeed();
+				}
+
+			}
+			else if (ballPosition.x > oppositeCorner.x)
+			{
+				if (ball->GetSpeed().x < 0)
+				{
+					ball->ReverseXSpeed();
+				}
+			}
+
+			if (ballPosition.y < pos.y)
+			{
+				if (ball->GetSpeed().y > 0)
+				{
+					ball->ReverseYSpeed();
+				}
+			}
+			else if (ballPosition.y > oppositeCorner.y)
+			{
+				if (ball->GetSpeed().y < 0)
+				{
+					ball->ReverseYSpeed();
+				}
+			}
+		}
+	}
+}
+
+void Brick::Draw()
+{
+	DrawRectangle(pos.x, pos.y, width, height, ORANGE);
+}
+
+Vector2 Brick::GetPos()
+{
+	return pos;
+}
+
+float Brick::GetWidth()
+{
+	return width;
+}
+
+float Brick::GetHeight()
+{
+	return height;
+}
+
+bool Brick::GetIsActive()
+{
+	return isActive;
+}
+
+void Brick::Update(Ball* ball)
+{
+	CheckBallCollision(ball);
+}
---
+++ Final_Desarrollo1/Final_Desarrollo1/Brick.h
@@ -0,0 +1,22 @@
+#pragma once
+#include"raylib.h"
+#include"Ball.h"
+class Brick
+{
+private:
+	Vector2 pos;
+	float width;
+	float height;
+	bool isActive;
+public:
+	Brick(float width, float height, Vector2 pos);
+	~Brick();
+	void CheckBallCollision(Ball* ball);
+	void Draw();
+	Vector2 GetPos();
+	float GetWidth();
+	float GetHeight();
+	bool GetIsActive();
+	void Update(Ball* ball);
+};
+
